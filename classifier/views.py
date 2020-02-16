9189914bb2db992de41e2e9c59358e5f49d99b55
Zetinator <erick_zetina@hotmail.com>
2020-02-15 21:14:52 -0600

    now 2 pages... one to drop the image and one to tell the result

--- classifier/views.py
+++ classifier/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
-# http://{host}/
+# http://{host}/classifier
     """page to drop the car pictures...
     """
     try:
@@ -15,8 +15,20 @@ def index(request):
 
 @csrf_exempt
 def predict(request):
+# http://{host}/classifier
+    """page to load the prediction...
+    """
     try:
         context = {}
+        context['name'] = 'erick quiere mucho a su Marion :('
+        if request.method == 'POST':
+            if 'image' not in request.FILES['file'].content_type:
+                return HttpResponse(status=415)
+            # prediction using our network starts here...
+            context['name'] = request.FILES['file'].name
+            context['prediction'] = 'Okay you got me... I dont know that car :('
+            img = request.FILES['file'].read()
+            print(context)
     except Exception as e:
         raise HttpResponse(status=500)
-    return HttpResponse("si...esta bien", status=200)
+    return render(request, 'classifier/prediction.html', context)
